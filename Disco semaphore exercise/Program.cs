08c121ce416dfcb6ca900ec436e85862c0ad4191
Mithrall <kenneth-dorn@hotmail.com>
2017-08-18 09:28:33 +0200

    Add project files.

---
+++ Disco semaphore exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disco_semaphore_exercise {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
